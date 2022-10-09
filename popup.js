8a390f3e4feb14c28ae4561b6ac5b61964c098d7
Eilphy_ <75255501+qwq0867@users.noreply.github.com>
2022-10-09 20:44:15 +0800

    0.91

--- popup.js
+++ popup.js
@@ -1,4 +1,7 @@
-var isDark=false
+var isDark=false;
+let myButton=document.getElementById("a1");
+myButton.onclick=changeImg;
+//myButton.onclick=Darkmode;
 function changeImg()
 {
     if(isDark==false)
@@ -7,11 +10,18 @@ function changeImg()
         document.body.style.backgroundColor="#222222";
         document.getElementById("popspeed").style.backgroundColor="#666666";
         document.getElementById("popspeed").style.borderColor="#444444";
-        document.getElementById("text1").style.color="#AAAAAA"
-        document.getElementById("text2").style.color="#AAAAAA"
-        document.getElementById("text3").style.color="#AAAAAA"
-        document.body.style.borderColor="#000000"
+        document.getElementById("text1").style.color="#AAAAAA";
+        document.getElementById("text2").style.color="#AAAAAA";
+        document.getElementById("text3").style.color="#AAAAAA";
+        document.body.style.borderColor="#000000";
+        document.getElementsByClassName
         isDark=true;
+        chrome.tabs.query({active:true,currentWindow:true},function(tabs){
+            chrome.tabs.executeScript(tabs[0].id,{code:
+                "document.getElementById('i_cecream').style.backgroundColor='black';\
+                document.getElementsByClassName('bili-header__channel').style.backgroundColor=black;"
+            })
+        })
     }
     else
     {
@@ -19,16 +29,19 @@ function changeImg()
         document.body.style.backgroundColor="#FFFFFF";
         document.getElementById("popspeed").style.backgroundColor="#FFFFFF";
         document.getElementById("popspeed").style.borderColor="#DDDDDD";
-        document.getElementById("text1").style.color="#000000"
-        document.getElementById("text2").style.color="#000000"
-        document.getElementById("text3").style.color="#000000"
-        document.body.style.borderColor="#DDDDDD"
+        document.getElementById("text1").style.color="#000000";
+        document.getElementById("text2").style.color="#000000";
+        document.getElementById("text3").style.color="#000000";
+        document.body.style.borderColor="#DDDDDD";
         isDark=false;
+        chrome.tabs.query({active:true,currentWindow:true},function(tabs){
+            chrome.tabs.executeScript(tabs[0].id,{code:
+                "document.getElementById('i_cecream').style.backgroundColor='white';"
+            })
+        })
     }
 }
 function Darkmode()
 {
-
+    
 }
-let myButton=document.getElementById("a1");
-myButton.onclick=changeImg;
